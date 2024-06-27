39d19f46d7e700b8c70af4b21c08ffa6a95eba89
Arthur Elmes <arthur.elmes@gmail.com>
2024-06-27 12:42:49 -0400

    Initial pm monitoring code

---
+++ aq_monitor/aqs.py
@@ -0,0 +1,17 @@
+from sds011 import SDS011
+from datetime import datetime
+import time
+import pprint
+
+def measure_pm25():
+    sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
+    reading = sensor.query()
+    return {"pm2.5": reading[0], "pm10": reading[1]}
+    
+
+if __name__ == "__main__":
+    data = dict()
+    for i in range(10):
+        data[datetime.strftime(datetime.now(), "%Y%m%dT:%H:%M:%S.%f")] = measure_pm25()
+        time.sleep(1)
+    pprint.pprint(data)
